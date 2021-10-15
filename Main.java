cae0266fb2d49346488f40f53c6fa7f249b02114
Calcium_collision <kolian200224@gmail.com>
2021-10-15 11:50:04 +0300

    More fixes

--- Main.java
+++ Main.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Constants for convenience
     final static String space = " ";
     final static String doubleSpaces = "  ";
 
     public static void main(String[] args) {
+        // Initialize Scanner
         Scanner in = new Scanner(System.in);
-    System.out.println("Write the sentence: ");
-	 String sentence = deleteMiddleSpacesRecusrsive(in.nextLine().trim());
-     System.out.println(Arrays.toString(sentence.split(" ")));
+
+        // What do i want from the user
+        System.out.println("Write the sentence: ");
+
+        // Got String from user
+        String gotString = in.nextLine();
+
+        // trim() and send String to method deleteMiddleSpacesRecusrsive
+	    String sentence = deleteMiddleSpacesRecusrsive(gotString.trim());
+
+        // Split sentence by spaces to array, than use Arrays.toString() to print array in human species
+        System.out.println(Arrays.toString(sentence.split(" ")));
 
     }
 
